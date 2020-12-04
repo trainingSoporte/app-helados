c041a431da0bca065104dee564495baa42eb8888
pipoadri <AOJEDA@claro.com.ar>
2020-12-04 12:02:43 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let gusto = document.getElementById('8');
+
+console.log(gusto.children[2].style);
+
+// gusto.children[2].style.color='red';
+
+console.log(gusto.children[2].classList);
+
+gusto.children[2].classList.add('cucharitaSinGusto')
