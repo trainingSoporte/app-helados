0c6ffe6fad153cec67f0d860132324cbded30f3e
Adrian Ojeda <pipoadri@gmail.com>
2022-09-24 22:51:12 -0300

    agrego cambios a edit

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-let gusto = document.getElementById('8');
 
-console.log(gusto.children[2].style);
 
-// gusto.children[2].style.color='red';
+let navBar = document.getElementsByClassName('navBar');
+console.log(navBar);
 
-console.log(gusto.children[2].classList);
+//Archivo
+navBar.archivo.addEventListener('click',(e)=>{
+    e.preventDefault();
+    e.stopImmediatePropagation();
+})
 
-gusto.children[2].classList.add('cucharitaSinGusto')
+
+//Editar
+let gustos = document.getElementsByClassName('gusto');
+
+navBar.editar.addEventListener('click',(e)=>{
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    let isActive = (navBar.editar.classList.toggle('optionActive'));
+    editMode(gustos,isActive);
+})
+// let editOption = document.getElementsByClassName('edit');
+// console.log(editOption);
+
+// console.log(gustos);
+
+for (const gusto of gustos) {
+    for (const child of gusto.children) {
+        // console.log(child);
+        if(child.classList.contains('foto')) child.classList.toggle('foto--editInactive');
+        if(child.classList.contains('box')) child.classList.toggle('box--editInactive');
+        if(child.classList.contains('cucharita')) child.classList.toggle('cucharita--editInactive');
+    }
+}
+    
+
+//Facturar
+navBar.facturar.addEventListener('click',(e)=>{
+    e.preventDefault();
+    e.stopImmediatePropagation();
+})
---
+++ scripts/edit.js
@@ -0,0 +1,84 @@
+
+
+//main -- let gustos = document.getElementsByClassName('gusto');
+
+
+const handleClickSave = (e) => {
+    console.log('save');
+    e.stopImmediatePropagation();
+    e.path.forEach(element => {
+        //     console.log(element);
+        if (element.classList != undefined && element.classList.contains('gusto')) {
+
+            element.addEventListener('click', handleClickEdit);
+            for (const child of element.children) {
+                // console.log(child);
+                if (child.classList.contains('foto')) child.classList.toggle('foto--editActive');
+                if (child.classList.contains('box')) {
+                    child.lastElementChild.textContent = child.firstElementChild.value;
+                    child.classList.toggle('box--editActive');
+                };
+                if (child.classList.contains('cucharita')) {
+                    child.removeEventListener('click', handleClickSave);
+                    child.classList.toggle('cucharita--editActive');
+                }
+
+            }
+
+        }
+    });
+
+}
+
+const handleClickEdit = (e) => {
+    console.log(e);
+    e.stopImmediatePropagation();
+    console.log(e.path);
+    e.path.forEach(element => {
+        if (element.classList != undefined && element.classList.contains('gusto')) {
+            element.removeEventListener('click', handleClickEdit);
+            for (const child of element.children) {
+                // console.log(child);
+                if (child.classList.contains('foto')) child.classList.toggle('foto--editActive');
+                if (child.classList.contains('box')) {
+                    child.firstElementChild.value = child.lastElementChild.textContent;
+                    child.classList.toggle('box--editActive');
+                }
+                if (child.classList.contains('cucharita')) {
+                    child.addEventListener('click', handleClickSave);
+                    child.classList.toggle('cucharita--editActive');
+                }
+
+            }
+        }
+    });
+
+
+
+}
+
+
+const editMode = (gustos, isActive) => {
+
+    for (const gusto of gustos) {
+        console.log(gusto);
+        if (isActive)
+            gusto.addEventListener('click', handleClickEdit);
+        else {
+            gusto.removeEventListener('click', handleClickEdit);
+
+            for (const child of gusto.children) {
+                // console.log(child);
+                if (child.classList.contains('foto')) child.classList.remove('foto--editActive');
+                if (child.classList.contains('box')) child.classList.remove('box--editActive');
+                if (child.classList.contains('cucharita')) {
+                    child.removeEventListener('click', handleClickSave);
+                    child.classList.remove('cucharita--editActive')
+                };
+            }
+        }
+    }
+}
+
+
+
